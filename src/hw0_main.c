b6694257df49a98e40ad7b57a00ce54eb0e3772e
Andrew Shi <164560703+andrewshi2027@users.noreply.github.com>
2024-08-28 17:16:16 +0000

    8/28

--- src/hw0_main.c
+++ src/hw0_main.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> //preprocessor directive
+
+//Constants
 #define ROWS 5
 #define COLS 5
 
@@ -9,16 +11,71 @@ char board[ROWS][COLS] = {
     {'o', '-', 'x', 'x', '-'}, 
     {'x', 'o', 'x', 'o', 'o'}};
 
-
-
 /* Hint:
-
 scanf(" %c", &choice); //this command will erase whitespace in the token selection
-
 */
 
+
 int main() {
-    
+
+    char piece;
+    int row, column;
+
+    //Print Board
+    for (int i = 0; i < 5; i++) {
+        printf("\n");
+        for (int j = 0; j < 5; j++) 
+        {
+            printf("%c ", board[i][j]);
+        }
+    }
+    printf("\n");
+
+
+    //User Input for Piece
+    while (1) {
+        printf("Choose a piece (x or o) or q to quit: ");
+        scanf("%c", &piece);
+        if (piece == 'x' || piece == 'o' || piece == 'q') {
+            break;
+        }
+        else {
+            printf("Invalid choice. ");
+        }
+    }
+
+    // If user chose "q" (quit)
+    if (piece == 'q') {
+        printf("Quitting the game.");
+        return 0;
+    }
+
+    //User Input for Row
+    while (1) {
+        printf("Choose a row (0-4): ");
+        scanf("%d", &row);
+        if (row >= 0 && row < ROWS) {
+            break;
+        }
+        else {
+            printf("Invalid choice. ");
+        }
+    }
+
+    //User Input for Column
+    while (1) {
+        printf("Choose a column (0-4): ");
+        scanf("%d", &column);
+
+        if (column >= 0 && column < COLS) {
+            break;
+        }
+        else {
+            printf("Invalid choice. ");
+        }
+    }
+
+    //If invalid input
     return 0;
 }
 
